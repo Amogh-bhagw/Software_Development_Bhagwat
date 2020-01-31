2a14465b624a8016455f257cbb787072568eb8d2
Amogh Bhagwat <bhagw018@umn.edu>
2020-01-31 08:25:45 -0600

    add(lab03): Adding lab03 deliverables

--- labs/lab03_class_basics/point2.cc
+++ labs/lab03_class_basics/point2.cc
@@ -6,15 +6,36 @@ using namespace std;
 float Point2::DistanceBetween(Point2 other_){
 	float x_distance = (other_.x_ - this->x_)*(other_.x_ - this->x_);
 	float y_distance = (other_.y_ - this->y_)*(other_.y_-this->y_);
-	return sqrtf(x_distance + y_distance);	
+	return sqrtf(x_distance + y_distance);
 }
 
 int Point2::Quadrant(){
+	float x_Point =  x_;
+	float y_Point = y_;
+	if(x_Point == 0 && y_Point == 0){
+		return 0;
+	}
+	if (x_Point < 0){
+		if(y_Point > 0){
+			return 2;
+		}
+		else{
+			return 3;
+		}
+	}
+	else{
+		if(y_Point > 0){
+			return 1;
+		}
+		else{
+			return 4;
+		}
+	}
 	return 0;
 }
 
 void Point2::Print(){
-	cout << "Hlel" ;
+	cout << "Point x: " << x_ << " Point y: " << y_ << endl;
 }
 
 Point2::Point2() {
@@ -26,4 +47,3 @@ Point2::Point2(float a, float b) {
 	x_ = a;
 	y_ = b;
 }
-
