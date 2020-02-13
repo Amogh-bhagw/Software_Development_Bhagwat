1e66c63caa2f6258340603fda6485845021af929
Amogh Bhagwat <bhagw018@umn.edu>
2020-02-13 16:05:56 -0600

    add(lab06): Adding lab06 deliverables

--- labs/lab06_gdb/main.cc
+++ labs/lab06_gdb/main.cc
@@ -24,7 +24,7 @@ void doInterestingThing(int yr) {
   array_len = NULL;
   d2 = Date(yr, 4, 19);
   d3 = d2.copy();
-  *array_len = 4;
+  array_len = new int(4);
 
   cout << "default date is: " << d1.show() << endl;
 
@@ -41,20 +41,28 @@ void doInterestingThing(int yr) {
   }
 
   // now show the dates accumulated in reverse order
-  for (int i = 0; i < *array_len; --i) {
+  for (int i = *array_len-1; i > -1; --i) {
     cout << "date is: " << birthday_list[i]->show() << endl;
   }
 
   // now set the dates to January 2
-  for (int i = 0; i <= *array_len; i++) {
+  for (int i = 0; i < *array_len; i++) {
     *birthday_list[i] = Date(1990+i, 1, 2);
   }
-  
+
   cout << "the first date is: " << birthday_list[0]->show() << endl;
+
+  for (int i=0;i<*array_len;i++) {
+    delete birthday_list[i];
+    delete[] birthday_list;
+  }
+  delete array_len;
 }
 
 int main() {
   doInterestingThing(1998);
   doInterestingThing(1999);
   cout << endl << "... Goodbye ..." << endl;
+
+
 }
