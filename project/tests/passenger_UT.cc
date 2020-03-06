69daf37a41ff5c56bfb47e8f2b7bb4a875480ed1
Amogh Bhagwat <bhagw018@umn.edu>
2020-03-05 18:40:19 -0600

    Add(bus_UT.cc, route_UT.cc, stop_UT.cc, passenger_UT.cc
    - Added the first tests.
    - Tested all public functions and constructor for passenger
    - Tested all return boolen functions and constructor for bus, stop, route

--- project/tests/passenger_UT.cc
+++ project/tests/passenger_UT.cc
@@ -17,7 +17,7 @@
  * Includes
  ******************************************************************************/
 #include <gtest/gtest.h>
-
+#include <sstream>
 #include <iostream>
 #include <string>
 #include <list>
@@ -37,20 +37,33 @@ class PassengerTests : public ::testing::Test {
 protected:
   PassengerLoader* pass_loader;
   PassengerUnloader* pass_unloader;
-  Passenger *passenger, *passenger1, *passenger2;
-    
+  Passenger *passenger, *passenger1, *passenger2, *passenger3, *passenger4;
+
   virtual void SetUp() {
     pass_loader = new PassengerLoader();
     pass_unloader = new PassengerUnloader();
+    passenger1 = new Passenger(2, "Amogh");
+    passenger = new Passenger();
+    passenger2 = new Passenger(100, "John");
+    passenger3 = new Passenger(-5, "Negative");
+    passenger4 = new Passenger(2000, "Extreme");
   }
 
   virtual void TearDown() {
     delete pass_loader;
     delete pass_unloader;
     delete passenger;
-    passenger = NULL;
+    delete passenger1;
+    delete passenger2;
+    delete passenger3;
+    delete passenger4;
     pass_loader = NULL;
     pass_unloader = NULL;
+    passenger = NULL;
+    passenger1 = NULL;
+    passenger2 = NULL;
+    passenger3 = NULL;
+    passenger4 = NULL;
   }
 };
 
@@ -59,10 +72,84 @@ protected:
  * Test Cases
  ******************************************************************************/
 TEST_F(PassengerTests, Constructor) {
-  passenger = new Passenger();
   EXPECT_EQ(passenger->IsOnBus(), false);
   passenger->GetOnBus();
   EXPECT_EQ(passenger->IsOnBus(), true);
+  EXPECT_EQ(passenger1->GetDestination(), 2) << "Check if set correctly";
+  EXPECT_EQ(passenger2->GetDestination(), 100) << "Check to see large number";
 };
 
+TEST_F(PassengerTests, GetOnBus) {
+  EXPECT_EQ(passenger->IsOnBus(), false);
+  passenger->GetOnBus();
+  EXPECT_EQ(passenger->IsOnBus(), true);
+  EXPECT_EQ(passenger->GetTotalWait(), 1) << "Intial on the bus";
+  passenger->Update();
+  EXPECT_EQ(passenger->GetTotalWait(), 2) << "after a update";
+  passenger1->GetOnBus();
+  EXPECT_EQ(passenger1->IsOnBus(), true);
+  EXPECT_EQ(passenger1->GetTotalWait(), 1);
+  passenger1->Update();
+  EXPECT_EQ(passenger1->GetTotalWait(), 2);
+  passenger1->GetOnBus();
+  EXPECT_EQ(passenger1->GetTotalWait(), 1) << "should reset to 1";
 
+};
+
+TEST_F(PassengerTests, GetTotalWait){
+  passenger->Update();
+  passenger->Update();
+  passenger->Update();
+  passenger->Update();
+  passenger->Update();
+  EXPECT_EQ(passenger->GetTotalWait(), 5) << "Alot of updates";
+  passenger->Update();
+  EXPECT_EQ(passenger->GetTotalWait(), 6);
+  EXPECT_EQ(passenger1->GetTotalWait(), 0) << "Intial zero wait time";
+  passenger2->Update();
+  passenger2->Update();
+  passenger2->GetOnBus();
+  passenger2->Update();
+  passenger2->Update();
+  EXPECT_EQ(passenger2->GetTotalWait(), 5) << "Seeing if time at stop and on bus is added";
+  EXPECT_EQ((passenger2->GetTotalWait()-3), 2) << "Subtracting time";
+  EXPECT_EQ((passenger2->GetTotalWait()+100), 105) << "Adding time";
+};
+
+TEST_F(PassengerTests, IsOnBus){
+    EXPECT_EQ(passenger->IsOnBus(), false);
+    EXPECT_EQ(passenger->IsOnBus(), false) << "checking again";
+    passenger->GetOnBus();
+    EXPECT_EQ(passenger->IsOnBus(), true);
+    passenger->GetOnBus();
+    EXPECT_EQ(passenger->IsOnBus(), true) << "checking if calling GetOnBus throws a error";
+
+};
+
+TEST_F(PassengerTests, GetDestination){
+  EXPECT_EQ(passenger3->GetDestination(), -5) << "Checking if negative will work";
+  EXPECT_EQ((passenger3->GetDestination() + 5), 0) << "Checking if Adding will work";
+  EXPECT_EQ(passenger->GetDestination(), -1) << "Intial setup number";
+  EXPECT_EQ(passenger1->GetDestination(), 2) << "Some different output";
+  EXPECT_EQ(passenger2->GetDestination(), 100);
+  EXPECT_EQ(passenger4->GetDestination(), 2000) << "Large number";
+};
+
+ TEST_F(PassengerTests, Report){
+  string expected_output = "Name: Nobody\nDestination: -1\nTotal Wait: 0\n\tWait at Stop: 0\n\tTime on bus: 0\n";
+  string expected_output1 = "Name: Amogh\nDestination: 2\nTotal Wait: 0\n\tWait at Stop: 0\n\tTime on bus: 0\n";
+//   passenger1 = new Passenger(2, "Amogh");
+    testing::internal::CaptureStdout();
+    passenger->Report(cout);
+    string output = testing::internal::GetCapturedStdout();
+
+    testing::internal::CaptureStdout();
+    passenger1->Report(cout);
+    string output1 = testing::internal::GetCapturedStdout();
+
+    int p1 = output.find(expected_output);
+    int p2 = output1.find(expected_output1);
+
+    EXPECT_GE(p1, 0);
+    EXPECT_GE(p2, 0);
+ };
